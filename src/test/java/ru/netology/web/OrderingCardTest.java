910d526fdc1ceaa8ea9bb81654d1d3d81517a74b
Igor <TimoshenkoII@yandex.com>
2023-05-27 10:49:11 +0300

    Changes commit

--- src/test/java/ru/netology/web/OrderingCardTest.java
+++ src/test/java/ru/netology/web/OrderingCardTest.java
@@ -15,7 +15,7 @@ public class OrderingCardTest {
     void orderOfTheCardIsTheTestFirst() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Велев Максим");
-        $("[data-test-id=phone] input").setValue("+796458519534");
+        $("[data-test-id=phone] input").setValue("+79645851953");
         $("[data-test-id=agreement]").click();
         $("button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
